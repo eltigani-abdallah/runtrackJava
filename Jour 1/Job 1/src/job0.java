e7a3aee4e3800762167dd3a9aabd979c179cb056
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-06-18 10:12:11 +0200

    jour 1 job 0

---
+++ Jour 1/Job 1/src/job0.java
@@ -0,0 +1,21 @@
+class job0{
+    public static void main(String[] args) {
+
+        char letter = 'h';
+        String hello = "Hello";
+        int TOTO = 3;
+        //int TOTO = 3.817 ne marche pas à cause de difference de types; 3.817 n'es PAS un int donc on voir un erreur de compilation
+        long longNum = 5435;
+        float floaty = 26.7f;
+        boolean makesSense = false;
+
+        System.out.println(letter);
+        System.out.println(hello);
+        System.out.println(TOTO);
+        System.out.println(longNum);
+        System.out.println(floaty);
+        System.out.println(makesSense);
+
+
+    }
+}
